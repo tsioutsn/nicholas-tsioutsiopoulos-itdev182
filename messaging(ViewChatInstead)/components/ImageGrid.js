35cbdf33acab1b0ba1929e56be0585ca5d3483a2
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2021-03-07 20:53:52 -0600

    Add files via upload

---
+++ messaging(ViewChatInstead)/components/ImageGrid.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Grid from './Grid';
+import { Permissions } from 'expo';
+import { CameraRoll, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+const keyExtractor = ({ uri }) => uri;
+
+export default class ImageGrid extends React.Component {
+    static propTypes = {
+        onPressImage: PropTypes.func,
+    };
+
+    static defaultProps = {
+        onPressImage: () => {},
+    };
+
+    loading = false;
+    cursor = null;
+    state = {
+        images: [],
+    };
+
+    componentDidMount() {
+        this.getImages();
+    }
+
+    getImages = async after => {
+        if (this.loading) return;
+
+        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (status !== 'granted') {
+            console.log('Camera roll permission Denied');
+            return;
+        }
+
+        this.loading = true;
+
+        const results = await CameraRoll.getPhotos({
+            first: 20,
+            after,
+        });
+
+        const { edges, page_info: { has_next_page, end_cursor } } = results;
+        const loadedImages = edges.map(item => item.node.image);
+
+        this.setState(
+            {
+                images: this.state.images.concat(loadedImages),
+            },
+            () => {
+                this.loading = false;
+                this.cursor = has_next_page ? end_cursor : null;
+            },
+        );
+    };
+
+    getNextImages = () => {
+        if (!this.cursor) return;
+        this.getImages(this.cursor);
+    };
+
+    renderItem = ({ item: { uri }, size, marginTop, marginLeft }) => {
+        const { onPressImage } = this.props;
+        const style = {
+            width: size,
+            height: size,
+            marginLeft,
+            marginTop,
+        };
+
+        return (
+            <TouchableOpacity
+                key={uri}
+                activeOpacity={0.75}
+                onPress={() => onPressImage(uri)}
+                style={style}
+                >
+                <Image source={{ uri }} style={styles.image} />
+                </TouchableOpacity>
+        );
+    };
+
+    render() {
+        const { images } = this.state;
+
+        return (
+            <Grid
+                data={images}
+                renderItem={this.renderItem}
+                keyExtractor={keyExtractor}
+                onEndReached={this.getNextImages}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+    },
+});
